406713d904567ae0cd3c63de4f53340316f455e2
Анастасия Маругина <45240148+marugish@users.noreply.github.com>
2024-04-24 00:27:39 +0300

    Спринт 8: домашнее задание v.1

---
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,19 @@
+package com.example.playlistmaker
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        val buttonBack = findViewById<Button>(R.id.back_settings_button)
+
+        buttonBack.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+}
