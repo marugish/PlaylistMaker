e4c1d31b2eecf0db866bb193f6ab0da4a2765c02
Анастасия Маругина <45240148+marugish@users.noreply.github.com>
2024-08-08 23:12:43 +0300

    Спринт 11: v.1 без placeholder

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -4,17 +4,36 @@ import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 class SearchActivity : AppCompatActivity() {
+
+    private val searchBaseUrl = "https://itunes.apple.com"
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(searchBaseUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+    private val itunesService = retrofit.create(ItunesApi::class.java)
+
+    private val results = ArrayList<Track>()
+    private val adapter = TrackAdapter(results)
+
     private var searchQuery: String = SEARCH_QUERY
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -35,12 +54,16 @@ class SearchActivity : AppCompatActivity() {
         inputEditText.setText(searchQuery)
     }
 
+    private lateinit var inputEditText: EditText
+    private lateinit var clearButton: ImageView
+    private lateinit var recycler: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
-        val inputEditText = findViewById<EditText>(R.id.search_edit_text)
-        val clearButton = findViewById<ImageView>(R.id.search_clear_button)
+        inputEditText = findViewById(R.id.search_edit_text)
+        clearButton = findViewById(R.id.search_clear_button)
 
         val toolbarBack = findViewById<Toolbar>(R.id.toolbar_search)
         toolbarBack.setNavigationOnClickListener {
@@ -61,6 +84,8 @@ class SearchActivity : AppCompatActivity() {
             }
         }
 
+
+
         val simpleTextWatcher = object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
                 searchQuery = s.toString()
@@ -76,11 +101,20 @@ class SearchActivity : AppCompatActivity() {
         }
         inputEditText.addTextChangedListener(simpleTextWatcher)
 
-        val recycler = findViewById<RecyclerView>(R.id.track_recycle_view)
+        recycler = findViewById(R.id.track_recycle_view)
         recycler.layoutManager = LinearLayoutManager(this)
-        recycler.adapter = TrackAdapter(
-            createTracks()
-        )
+        recycler.adapter = adapter
+
+        inputEditText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                if (inputEditText.text.isNotEmpty()) {
+                    Toast.makeText(applicationContext, "Выполняем запрос", Toast.LENGTH_LONG).show()
+                    search()
+                    true
+                }
+            }
+            false
+        }
 
     }
 
@@ -90,6 +124,42 @@ class SearchActivity : AppCompatActivity() {
         } else {
             View.VISIBLE
         }
+    }
+
+    private fun search() {
+        itunesService.searchTrack(inputEditText.text.toString())
+            .enqueue(object : Callback<TracksResponse> {
+                override fun onResponse(call: Call<TracksResponse>,
+                                        response: Response<TracksResponse>
+                ) {
+                    when (response.code()) {
+
+                        200 -> {
+                            if (response.body()?.results?.isNotEmpty() == true) {
+                                results.clear()
+                                results.addAll(response.body()?.results!!)
+                                adapter.notifyDataSetChanged()
+                                Toast.makeText(applicationContext, "200: ОК", Toast.LENGTH_LONG).show()
+                                //showMessage("", "")
+                            } else {
+                                print(response.body().toString())
+                                Toast.makeText(applicationContext, "200: BAD, ничего не найдено", Toast.LENGTH_LONG).show()
+                                Log.e("Retrofit", "Ошибка: " + response.body().toString());//showMessage(getString(R.string.nothing_found), "")
+                            }
+
+                        }
+                        else -> Toast.makeText(applicationContext, "On Response: что-то пошло не так", Toast.LENGTH_LONG).show()
+                        //showMessage(getString(R.string.something_went_wrong), response.code().toString())
+                    }
+
+                }
+
+                override fun onFailure(call: Call<TracksResponse>, t: Throwable) {
+                    Toast.makeText(applicationContext, "On Failure: что-то пошло не так", Toast.LENGTH_LONG).show()
+                    //showMessage(getString(R.string.something_went_wrong), t.message.toString())
+                }
+
+            })
     }
 
 }
---
+++ app/src/main/java/com/example/playlistmaker/TracksResponse.kt
@@ -0,0 +1,3 @@
+package com.example.playlistmaker
+
+class TracksResponse(val results: ArrayList<Track>)
